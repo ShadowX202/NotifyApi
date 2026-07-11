58f4e0148c2057e2d65934a0ebdee92e2ca63b6e
ShadowX202 <shadow.x2029324@gmail.com>
2026-07-11 15:39:54 +0200

    .

--- src/main/java/de/ShadowX202/notify/NotificationSetting.java
+++ src/main/java/de/ShadowX202/notify/NotificationSetting.java
@@ -5,6 +5,6 @@ import de.ShadowX202.notify.notification.NotificationLevel;
 public interface NotificationSetting {
 
     boolean isEnabled(String type, NotificationLevel level);
-
+    boolean isEnabled(String type);
 
 }
